4c4d65fb080048d9a23587d69d895740434864f7
선환 <znzn9292@gmail.com>
2020-05-26 01:28:01 +0900

    2020-05-26 1.LomBok Constructor JUnit Test 2.LomBok Error : Gradle down grade

--- src/test/java/com/znzn/book/springboot/web/dto/HelloResponseDtoTest.java
+++ src/test/java/com/znzn/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -1,8 +1,11 @@
 package com.znzn.book.springboot.web.dto;
 
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 public class HelloResponseDtoTest {
 
