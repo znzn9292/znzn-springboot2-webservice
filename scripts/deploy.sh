fd748cc3cc260f22a19b292a5fbbcadc97d6368d
jungsunhwan <znzn9292@gmail.com>
2020-06-08 10:53:38 +0900

    2020.06-08 / 배포 자동화 구성 test4

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -33,7 +33,7 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-ohup java -jar \
+nohup java -jar \
     -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
     -Dspring.profiles.active=real \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
